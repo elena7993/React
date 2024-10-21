dda15eeb60a99bff0d8a0294346dd479be3e8580
elena7993 <elenalee7993@gmail.com>
2024-10-21 23:54:16 +0900

    21_10_24

--- src/components/ex_05/Ex05.js
+++ src/components/ex_05/Ex05.js
@@ -1,56 +1,100 @@
 import styled from "styled-components";
 
 const Wrap = styled.div`
-  width: 500px;
-  height: 800px;
-  background-color: lightgrey;
-  border: 1px solid #000;
-
-  div {
-    width: 480px;
-    height: 400px;
-    /* background-color: salmon; */
-    border-radius: 20px;
-    overflow: hidden;
-    margin-left: 10px;
-    margin-top: 10px;
-  }
-
-  img {
-    object-fit: fill;
-  }
-
-  p {
-    font-size: 18px;
-    font-weight: 700;
-    margin-top: 15px;
-    margin-left: 10px;
-  }
-
-  /* div {
-    width: 400px;
-    height: 5px;
-    background-color: gray;
-  } */
+  width: 330px;
+  height: 600px;
+  border: 3px solid #000;
+  border-radius: 10px;
+  margin: 0 auto;
 `;
+
+const MusicImg = styled.div`
+width: 300px;
+height: 300px;
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 20px;
+  margin: 15px;
+}
+`;
+
+const MusicInfo = styled.div`
+margin-top: 30px;
+margin-left: 15px;
+.title {
+  font-size: 20px;
+  font-weight: 700;
+}
+
+.desc {
+  font-size: 18px;
+  color: salmon;
+  margin-top: 5px;
+}
+`;
+
+const PlayBar = styled.div`
+width: 300px;
+height: 5px;
+background-color: gray;
+margin-left: 15px;
+margin-top: 20px;
+
+`;
+
+const BtnWrap = styled.div`
+display: flex;
+justify-content: space-around;
+margin-top: 30px;
+
+i{
+  font-size: 24px;
+  cursor: pointer;
+}
+`;
+
+const Volume = styled.div`
+width: 150px;
+height: 5px;
+background-color: gray;
+margin-top: 50px;
+margin-left: 15px;
+`;
+
+
+
 
 const Ex05 = () => {
   return (
     <>
       <Wrap>
-        <div>
+
+        <MusicImg>
           <img
-            src="https://m.simf.kr/web/product/big/202109/ddfcdb99dc21ab5d0688c2a8cd4edbba.jpg"
+            src="https://i1.sndcdn.com/artworks-Eke4dWZTIrXCkXPW-hX2ihg-t500x500.jpg"
             alt="img"
           />
-        </div>
-        <p>
-          Beetoveen Piano Concerte No. 1<br />
-          in C Major, Op I5:11. Rondo. <br />
-          Allegro sehezando
-        </p>
+        </MusicImg>
 
-        <div></div>
+        <MusicInfo>
+          <p className="title">Blilngding Lights</p>
+          <p className="desc">The Weeknd</p>
+        </MusicInfo>
+
+        <PlayBar>
+          <div></div>
+        </PlayBar>
+
+        <BtnWrap>
+        <i class="fa-solid fa-backward"></i>
+        <i class="fa-solid fa-pause"></i>
+        <i class="fa-solid fa-forward"></i>
+        </BtnWrap>
+
+        <Volume></Volume>
+      
       </Wrap>
     </>
   );
